c5de2e45b159224aff0e681f1098612f6db7ad2c
Sergei Izmalkov <bushkar@engineer.com>
2026-01-20 20:10:05 +0200

    Login Positive Test

--- src/test/java/ui_tests/LoginTests.java
+++ src/test/java/ui_tests/LoginTests.java
@@ -1,8 +1,10 @@
 package ui_tests;
 
+import dto.User;
 import manager.AppManager;
 import org.testng.annotations.Test;
 import pages.HomePage;
+import pages.LoginPage;
 
 public class LoginTests extends AppManager {
 
@@ -11,5 +13,18 @@ public class LoginTests extends AppManager {
         System.out.println("first test");
         HomePage homePage = new HomePage(getDriver());
         homePage.clickBtnLogin();
+        LoginPage loginPage = new LoginPage(getDriver());
+        loginPage.typeLoginRegistrationForm("family@mail.ru", "Family123!");
+        loginPage.clickBtnLoginForm();
+    }
+
+    @Test
+    public void loginPositiveTestWithUser() {
+        User user = new User("family@mail.ru", "Family123!");
+        HomePage homePage = new HomePage(getDriver());
+        homePage.clickBtnLogin();
+        LoginPage loginPage = new LoginPage(getDriver());
+        loginPage.typeLoginRegistrationFormWithUser(user);
+        loginPage.clickBtnLoginForm();
     }
 }
