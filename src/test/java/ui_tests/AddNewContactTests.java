9492bc9a291060629fc3f084df53e939253c22a3
Sergei Izmalkov <bushkar@engineer.com>
2026-01-30 16:43:09 +0200

    Add New Contact Positive Test

---
+++ src/test/java/ui_tests/AddNewContactTests.java
@@ -0,0 +1,33 @@
+package ui_tests;
+
+import manager.AppManager;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import pages.*;
+import utils.HeaderMenuItem;
+
+import static pages.BasePage.clickButtonHeader;
+import static utils.ContactFactory.*;
+
+
+public class AddNewContactTests extends AppManager {
+    HomePage homePage;
+    LoginPage loginPage;
+    ContactsPage contactsPage;
+    AddPage addPage;
+
+    @BeforeMethod
+    public void login() {
+        homePage = new HomePage(getDriver());
+        loginPage = clickButtonHeader(HeaderMenuItem.LOGIN);
+        loginPage.typeLoginRegistrationForm("family@mail.ru", "Family123!");
+        loginPage.clickBtnLoginForm();
+        contactsPage = new ContactsPage(getDriver());
+        addPage = clickButtonHeader(HeaderMenuItem.ADD);
+    }
+
+    @Test
+    public void addNewContactPositiveTest() {
+        addPage.typeContactForm(positiveContact());
+    }
+}
