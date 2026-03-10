d40adc8b490eaad2a7ccff3e2c78325461c1a051
Sergei Izmalkov <bushkar@engineer.com>
2026-03-10 19:08:56 +0200

    Cross Browser Testing

--- src/main/java/manager/AppManager.java
+++ src/main/java/manager/AppManager.java
@@ -2,6 +2,8 @@ package manager;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.events.EventFiringDecorator;
 import org.openqa.selenium.support.events.WebDriverListener;
 import org.slf4j.Logger;
@@ -17,6 +19,7 @@ import java.time.LocalTime;
 public class AppManager {
     public final static Logger logger =
             LoggerFactory.getLogger(AppManager.class);
+    static String browser = System.getProperty("browser", "chrome");
     private WebDriver driver;
 
     public WebDriver getDriver() {
@@ -26,7 +29,21 @@ public class AppManager {
     @BeforeMethod(alwaysRun = true)
     public void setup() {
         logger.info("Start testing " + LocalDate.now() + " : " + LocalTime.now());
-        driver = new ChromeDriver();
+//        driver = new ChromeDriver();
+        switch (browser.toLowerCase()) {
+            case "firefox":
+                driver = new FirefoxDriver();
+                System.out.println("Use FireFox");
+                break;
+            case "edge":
+                driver = new EdgeDriver();
+                System.out.println("Use Edge");
+                break;
+            case "chrome":
+                driver = new ChromeDriver();
+                System.out.println("Use Chrome");
+                break;
+        }
         driver.manage().window().maximize();
         driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(10));
 //        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
@@ -34,8 +51,8 @@ public class AppManager {
         driver = new EventFiringDecorator<>(webDriverListener).decorate(driver);
     }
 
-//    @AfterMethod(enabled = false)
-@AfterMethod(enabled = true, alwaysRun = true)
+    //    @AfterMethod(enabled = false)
+    @AfterMethod(enabled = true, alwaysRun = true)
     public void tearDown() {
         logger.info("Stop testing " + LocalDate.now() + " : " + LocalTime.now());
         if (driver != null)
